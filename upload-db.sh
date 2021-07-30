3e54f4347706cf939cbf757108fc3d4b84d00d72
arozhdan <halala30@gmail.com>
2021-07-30 15:35:15 +0200

    123

--- upload-db.sh
+++ upload-db.sh
@@ -26,23 +26,5 @@ rm dump.sql
 
 echo "Copying staging db... \n\n"
 
-# ssh root@64.225.103.36 " docker exec -t $REPO'_postgres_1' psql -U strapi -d strapi -c '
-#   DO \$\$  
-#     DECLARE 
-#       r RECORD;
-#     BEGIN
-#     FOR r IN 
-#       (
-#         SELECT table_name
-#         FROM information_schema.tables 
-#         WHERE table_schema=current_schema()
-#       ) 
-#     LOOP
-#         EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.table_name) || ' CASCADE';
-#     END LOOP;
-#   END \$\$ ;
-#   '
-# "
-ssh root@64.225.103.36 
-
 ssh staging@64.225.103.36 "./erase-meet-staging.db"
+ssh staging@64.225.103.36 " /backup/dump_staging.sql | docker exec -i $REPO'_postgres_1' psql -U strapi"
