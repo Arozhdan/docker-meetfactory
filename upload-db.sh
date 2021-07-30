5adddd218734911cd5bb8f3f39a410a02da1ec17
arozhdan <halala30@gmail.com>
2021-07-30 14:04:48 +0200

    update

--- upload-db.sh
+++ upload-db.sh
@@ -1,7 +1,7 @@
 git checkout develop
 git add .
-echo "Print commit message:"
-read message
+echo "Enter commit message:"
+read message -r
 git commit -m $message 
 git push
 
@@ -19,9 +19,28 @@ echo "Done. Removing the sql file:
 "
 rm dump.sql
 
-REPO=$(node -p -e "require('./package.json').name")
+REPO=$(node -p -e "require('./package.json').name") 
 
 
+echo "Copying staging db... \n\n"
+ssh root@64.225.103.36 "
+  docker exec -t $REPO'_postgres_1' pg_dump -c -U strapi strap -c \"
+          DO \$\$ 
+            DECLARE 
+              r RECORD;
+            BEGIN
+            FOR r IN 
+              (
+                SELECT table_name
+                FROM information_schema.tables 
+                WHERE table_schema=current_schema()
+              ) 
+            LOOP
+               EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.table_name) || ' CASCADE';
+            END LOOP;
+          END \$\$ ;
+          \"
+"
 echo "Copying staging db... \n\n"
 ssh root@64.225.103.36 "
   docker exec -t $REPO'_postgres_1' pg_dump -c -U strapi strapi > /backup/dump_staging.sql
