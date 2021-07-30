f8b95ddb05f1f4b0b171bec3f402b9d1758c246c
arozhdan <halala30@gmail.com>
2021-07-30 14:42:10 +0200

    ad

--- upload-db.sh
+++ upload-db.sh
@@ -25,7 +25,7 @@ rm dump.sql
 
 
 echo "Copying staging db... \n\n"
-ssh root@64.225.103.36 \"
+ssh root@64.225.103.36 <<__END__
   docker exec -t $REPO'_postgres_1' psql -U strapi -d strapi -c "
           DO \$\$  
             DECLARE 
@@ -41,5 +41,6 @@ ssh root@64.225.103.36 \"
                EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.table_name) || ' CASCADE';
             END LOOP;
           END \$\$ ;
-          ";docker exec -t $REPO'_postgres_1' pg_dump -c -U strapi strapi > /backup/dump_staging.sql
-         \"
+          "; 
+  docker exec -t $REPO'_postgres_1' pg_dump -c -U strapi strapi > /backup/dump_staging.sql
+__END__
