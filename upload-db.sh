78cfb144de4da381e974bbe6aeb8e5d2c37d97bb
arozhdan <halala30@gmail.com>
2021-07-30 14:47:25 +0200

    ads

--- upload-db.sh
+++ upload-db.sh
@@ -47,4 +47,4 @@ command = "
 #   docker exec -t $REPO'_postgres_1' pg_dump -c -U strapi strapi > /backup/dump_staging.sql
 # __END__
 
-echo command | ssh root@64.225.103.36
+cat command | ssh root@64.225.103.36
